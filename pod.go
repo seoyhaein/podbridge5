6a4eb24aeb76d4a1da3575f69dc0d2dfe09e5fe7
seoyhaein <seoy@bio-caleb.com>
2025-07-20 17:05:11 +0900

    update

--- pod.go
+++ pod.go
@@ -2,3 +2,5 @@ package podbridge5
 
 // TODO 여기서 부터 시작.
 // pod 같은 경우는 일단 파이프라인의 규격이 정해지면 그것에 따라서 api 의 스펙이 확정될 것으로 판단.
+
+// infra container 확인 하고, volume 을 pod 에 넣어두는 것을 생각해보자. 실제로는 infra container 에 넣는 것인데..
