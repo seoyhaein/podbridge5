20ec2cdbd0143f700e6e4f88797b3a3c5a466a9a
seoyhaein <seoy@bio-caleb.com>
2025-07-20 20:31:15 +0900

    update

--- volume.go
+++ volume.go
@@ -31,43 +31,6 @@ const (
 	ModeOverwrite
 )
 
-// 주의 사항. 여기서 volumeName 생성이 전제되어 있다고 봐야 한다.
-
-// WithNamedVolume1 네임드 볼륨 마운트 옵션
-func WithNamedVolume1(volumeName, dest, subPath string, options ...string) ContainerOptions {
-	return func(spec *specgen.SpecGenerator) error {
-		// volumeName 이 비어있으면 익명 볼륨(anonymous volume)으로 처리
-		isAnonymous := false
-		// TODO 아래 내용 확인 해야함.
-		// 익명 보륨의 경우,일반적으로 익명 볼륨은 특정 컨테이너에 종속되어 사용되며, 해당 컨테이너가 삭제되면 함께 정리 되어야함.
-		// podman rm -v 이러한 방식으로 볼륨을 지울 수 있음.
-		if volumeName == "" {
-			isAnonymous = true
-		}
-
-		// 새로운 NamedVolume 객체 생성 (SubPath 필드 포함)
-		newVol := &specgen.NamedVolume{
-			Name:        volumeName,
-			Dest:        dest,
-			Options:     options,
-			IsAnonymous: isAnonymous,
-			SubPath:     subPath,
-		}
-
-		// 이미 같은 Dest 설정되어 있는지 확인
-		for _, vol := range spec.Volumes {
-			if vol.Dest == dest {
-				// 같은 Dest 이미 설정되어 있으면 덮어쓰지 않고 그냥 반환
-				return nil
-			}
-		}
-
-		// SpecGenerator 의 Volumes 필드에 새 볼륨 추가
-		spec.Volumes = append(spec.Volumes, newVol)
-		return nil
-	}
-}
-
 func WithNamedVolume(volumeName, dest, subPath string, options ...string) ContainerOptions {
 	return func(spec *specgen.SpecGenerator) error {
 		cleaned := strings.TrimSpace(volumeName)
